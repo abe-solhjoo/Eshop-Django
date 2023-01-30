9ae37c489dfd7cc05fb7e329d6a1c60bd373194e
Abe_soo <eb.solhjoo@gmail.com>
2023-01-30 22:03:23 +0330

    update products models

--- eshop_products/admin.py
+++ eshop_products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'title', 'price', 'active']
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)

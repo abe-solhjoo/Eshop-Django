1571d65cc6288af9309b498fc72a4602b0136315
Abe_soo <eb.solhjoo@gmail.com>
2023-01-30 20:34:29 +0330

    complete register form

--- eshop_account/forms.py
+++ eshop_account/forms.py
@@ -1,13 +1,54 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core import validators
 
 
 class LoginForm(forms.Form):
     user_name = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'لطفا نام کاربری خود را وارد نمایید'}),
-        label='نام کاربری'
+        label='نام کاربری',
+        validators=[
+            validators.MaxLengthValidator(limit_value=20,message='تعداد کاراکتر های وارد شده می بایست کمتر از 20 باشد')
+        ]
     )
 
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'لطفا کلمه ی عبور خود را وارد نمایید'}),
         label='کلمه ی عبور'
     )
+
+
+class RegisterForm(forms.Form):
+    user_name = forms.CharField(
+        widget=forms.TextInput(attrs={'place_holder': 'لطفا نام کاربری را وارد نمایید'}),
+        label='نام کاربری'
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'place_holder': 'لطفا ایمیل خود را وارد نمایید'}),
+        label='ایمیل'
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'place_holder': 'لطفا پسورد خود را وارد نمایید'}),
+        label='پسورد'
+    )
+    re_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'place_holder': 'لطفا تکرار پسورد خود را وارد نمایید'}),
+        label='تکرار پسورد'
+    )
+
+    def clean_user_name(self):
+        user_name = self.cleaned_data.get('user_name')
+        if_exists_user_by_username = User.objects.filter(username=user_name).exists()
+
+        if if_exists_user_by_username:
+            raise forms.ValidationError('نام کاربری وارد شده قبلا استفاده شده است')
+        return user_name
+
+    def clean_re_password(self):
+        password = self.cleaned_data.get('password')
+        re_password = self.cleaned_data.get('re_password')
+        if password != re_password:
+            raise forms.ValidationError('پسورد های وارد شده برابر نمی باشند')
+
+        return password
--- eshop_account/views.py
+++ eshop_account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 from django.contrib.auth import login, get_user_model, authenticate
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -35,7 +36,18 @@ def login_user(request):
 def register(request):
     if request.user.is_authenticated:
         return redirect('/')
-    context = {
 
+    register_form = RegisterForm(request.POST or None)
+
+    if register_form.is_valid():
+        user_name = register_form.cleaned_data.get('user_name')
+        password = register_form.cleaned_data.get('password')
+        email = register_form.cleaned_data.get('email')
+
+        User.objects.create_user(username=user_name, email=email, password=password)
+        return redirect('/login')
+
+    context = {
+        'register_form': register_form
     }
     return render(request, 'account/register.html', context)

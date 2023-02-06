004d21340e988767e527dd87de92d6a4ac545c5e
Abe_soo <eb.solhjoo@gmail.com>
2023-02-06 15:01:49 +0330

    add product gallery

--- eshop_products/admin.py
+++ eshop_products/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product
+from .models import Product, ProductGallery
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -13,3 +13,4 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductGallery)
--- eshop_products/models.py
+++ eshop_products/models.py
@@ -18,6 +18,13 @@ def upload_image_path(instance, filename):
     return f"products/{final_name}"
 
 
+def upload_gallery_image_path(instance, filename):
+    name, ext = get_filename_ext(filename)
+    final_name = f'{instance.id}-{instance.title}{ext}'
+    return f"products/galleries/{final_name}"
+
+
+
 # Create your models here.
 
 class ProductsManager(models.Manager):
@@ -64,3 +71,16 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return f"/products/{self.id}/{self.title.replace(' ', '-')}"
+
+
+class ProductGallery(models.Model):
+    title = models.CharField(max_length=150, verbose_name='عنوان')
+    image = models.ImageField(upload_to=upload_gallery_image_path, null=True, blank=True, verbose_name='تصویر')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='محصول')
+
+    class Meta:
+        verbose_name = 'گالری'
+        verbose_name_plural = 'گالری ها'
+
+    def __str__(self):
+        return self.title

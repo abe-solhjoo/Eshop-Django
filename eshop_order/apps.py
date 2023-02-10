bfec1a438d4554694e4a9f22b5d3aef64ca67f7a
Abe_soo <eb.solhjoo@gmail.com>
2023-02-10 18:12:18 +0330

    config app name

--- eshop_order/apps.py
+++ eshop_order/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class EshopOrderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'eshop_order'
+    verbose_name = 'ماژول سبد خرید'

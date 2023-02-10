de3164647223c81027aa51759a61f560e24f34e5
Abe_soo <eb.solhjoo@gmail.com>
2023-02-10 18:05:31 +0330

    create model for order and oreder detail

--- eshop_order/admin.py
+++ eshop_order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from eshop_order.models import Order, OrderDetail
+
+admin.site.register(Order)
+admin.site.register(OrderDetail)
--- eshop_order/models.py
+++ eshop_order/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+from eshop_products.models import Product
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(verbose_name='پرداخت شده')
+    payment_date = models.DateField(blank=True, null=True, verbose_name='تاریخ پرداخت')
+
+    class Meta:
+        verbose_name = 'سبد خرید'
+        verbose_name_plural = 'سبد های خرید'
+
+    def __str__(self):
+        return self.owner.get_full_name()
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='سبد خرید')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='محصولات')
+    price = models.IntegerField(verbose_name='قیمت محصول')
+    count = models.IntegerField(verbose_name='تعداد')
+
+    class Meta:
+        verbose_name = 'جزئیات محصول'
+        verbose_name_plural = 'اطلاعات جزئیات محصولات'
+
+    def __str__(self):
+        return self.product.title

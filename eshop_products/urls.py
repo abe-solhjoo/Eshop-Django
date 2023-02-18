db4a1c090a5a2f73f310f6001b593ee85a08da02
Abe_soo <eb.solhjoo@gmail.com>
2023-02-18 15:13:40 +0330

    fix search bug

--- eshop_products/urls.py
+++ eshop_products/urls.py
@@ -6,8 +6,8 @@ from .views import products, ProductsList, product_detail, SearchProductsView, P
 urlpatterns = [
     path('products-function', products),
     path('products', ProductsList.as_view()),
+    path('products/search', SearchProductsView.as_view()),
     path('products/<category_name>', ProductsListByCategory.as_view()),
     path('products/<productId>/<name>', product_detail),
-    path('products/search', SearchProductsView.as_view()),
     path('products_category_partial', products_categories_partial, name='products_categories_partial'),
 ]

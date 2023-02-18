e27966bb97f31bd48db427daed50dec7433cba22
Abe_soo <eb.solhjoo@gmail.com>
2023-02-18 13:28:33 +0330

    update paging

--- Eshop/views.py
+++ Eshop/views.py
@@ -1,4 +1,8 @@
+import itertools
+
 from django.shortcuts import render, redirect
+
+from eshop_products.models import Product
 from eshop_slider.models import Slider
 from eshop_settings.models import SiteSetting
 
@@ -22,11 +26,21 @@ def footer(request, *args, **kwargs):
     return render(request, 'shared/Footer.html', context)
 
 
+def my_grouper(n, iterable):
+    args = [iter(iterable)] * n
+    return ([e for e in t if e is not None] for t in itertools.zip_longest(*args))
+
+
 def home_page(request):
+    related_products = Product.objects.get_queryset().distinct()
+
+    grouped_related_products = my_grouper(3, related_products)
+
     sliders = Slider.objects.all()
     context = {
         'data': "new data",
-        'sliders': sliders
+        'sliders': sliders,
+        'grouped': grouped_related_products
     }
     return render(request, 'home_page.html', context)
 
